49fec38c64ed38cb6d5d3f2e8b0e37b174c8097a
WebCodersDeveloper <ibrohimahmedov869@gmail.com>
2024-04-09 21:29:14 +0500

    update 2.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,6 @@ export default {
       fontFamily: {
         roboto: ['Roboto','sans-serif'],
       },
-      backgroundImage:{
-        'heroBg' : "url('./src/image/heroBannner.png')",
-        'tree' : "url('./src/image/bgTree.svg ')",
-        'city' : "url('./src/image/bgCity.svg')",
-        'wildlife' : "url('./src/image/bgWildlife.svg')",
-        'donate' : "url('./src/image/donateBg.svg')",
-      },
       backgroundSize :{
         'standart' : "100%"
       },
